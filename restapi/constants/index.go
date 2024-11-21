6beac79035ae2143707c49f1a31e8e10eab39531
sivasenthil09 <sivasenthiludt@gmail.com>
2024-11-21 11:49:29 +0530

    Update index.go

--- restapi/constants/index.go
+++ restapi/constants/index.go
@@ -1,6 +1,9 @@
 package constants
 
-const ConnectionString ="mongodb+srv://siva:siva@banking.o62rjxe.mongodb.net/?retryWrites=true&w=majority"
+const ConnectionString ="mongodb+srv://siva:siva@banking.o62rjxe.mongodb.net/?retryWrites=true&w=majority&appName=Banking"
+// "mongodb+srv://siva:siva@banking.o62rjxe.mongodb.net/?retryWrites=true&w=majority"
 const Port =":4000"
-const DatabaseName ="NEW"
+const DatabaseName ="kavi"
 //"mongodb+srv://userone:L2ORM855sl5XzunU@cluster0.9eqsnyg.mongodb.net/"
+// mongodb+srv://siva:siva@banking.o62rjxe.mongodb.net/?retryWrites=true&w=majority&appName=Banking
+// const ConnectionString ="mongodb+srv://siva:siva@banking.o62rjxe.mongodb.net/?retryWrites=true&w=majority"
